15c9878cc326b5d3da6ec29f244486164b81cd96
Jelle De Moerloose <jelle.demoerloose@ugent.be>
2024-03-24 17:47:24 +0100

    effe commiten jeej

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, send_from_directory
-from api import patientAPI
 
+# from api import patientAPI
 
 
 app = Flask(__name__)
 
-app.register_blueprint(patientAPI)
+# app.register_blueprint(patientAPI)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return send_from_directory('static', 'index.html')
+    return send_from_directory("static", "index.html")
 
 
+@app.route("/chat")
+def chatbot():
+    return send_from_directory("static", "chat.html")
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
+
     app.run(debug=True)
